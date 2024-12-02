17a2e3ae9df5fd8a3a99d4cd29b4708e014f1294
GabrielFraga <gabrielbff2006@gmail.com>
2024-12-01 21:27:50 -0300

    .

--- q1/app.js
+++ q1/app.js
@@ -1,2 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
+const app = express()
+const port = 8081
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.post('/avg', (req,res) => {
+
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + 'index.html')
+})
+
+app.listen(port, () => {
+console.log('Servidor rodando na porta ' + port)
+})
