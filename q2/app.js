cec5755167a29edbbb13763af6d1dfbfd6a7aa00
GabrielFraga <gabrielbff2006@gmail.com>
2024-12-01 22:32:27 -0300

    q2

--- q2/app.js
+++ q2/app.js
@@ -8,6 +8,20 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
+app.post('/imc', (req,res) => {
+    var {weight, height} = req.body
+
+    weight = Number.parseFloat(weight)
+    height = Number.parseFloat(height)
+
+    if(weight <= 0 || height <= 0)
+        res.json({message: 'Peso ou altura inválido(s)!'})
+
+    var imc = weight / height**2
+
+    res.json({message: `Seu IMC é ${imc.toFixed([2])}`})
+})
+
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
