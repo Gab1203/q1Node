d09f477e2fc1729525866c35d3112ad80ddd6508
GabrielFraga <gabrielbff2006@gmail.com>
2024-12-02 22:53:01 -0300

    .

--- q8/app.js
+++ q8/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-
 const app = express()
 const port = 8081
 
@@ -9,30 +8,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
-app.post('/fileReader', (req,res) => {
-    var {file} = req.body
-
-    console.log(file)
-
-    var txt = new String(file).trim().toLowerCase()
-
-    var vogais = 0
-    var consoantes = 0
-    
-    for(let i=0; i < txt.length; i++){
-
-        if(txt.charAt(i) == 'a' | txt.charAt(i) == 'e' | txt.charAt(i) == 'i' | 
-        txt.charAt(i) == 'o' | txt.charAt(i) == 'u'){
-            vogais++;
-        }else {
-
-            
-            consoantes++
-        }
-    }
-
-    res.json({message: `O arquivo enviado possui ${vogais} vogais, ${consoantes} consoantes e demais caracteres!`})
-})
 
 
 app.get('/', (req,res) => {
--- q9/app.js
+++ q9/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+
 const app = express()
 const port = 8081
 
@@ -8,6 +9,30 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
+app.post('/fileReader', (req,res) => {
+    var {file} = req.body
+
+    console.log(file)
+
+    var txt = new String(file).trim().toLowerCase()
+
+    var vogais = 0
+    var consoantes = 0
+    
+    for(let i=0; i < txt.length; i++){
+
+        if(txt.charAt(i) == 'a' | txt.charAt(i) == 'e' | txt.charAt(i) == 'i' | 
+        txt.charAt(i) == 'o' | txt.charAt(i) == 'u'){
+            vogais++;
+        }else {
+
+            
+            consoantes++
+        }
+    }
+
+    res.json({message: `O arquivo enviado possui ${vogais} vogais, ${consoantes} consoantes e demais caracteres!`})
+})
 
 
 app.get('/', (req,res) => {
