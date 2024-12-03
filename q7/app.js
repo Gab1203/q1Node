50cad3859d91f8333603d79d3d3a9bb3fb7e9d78
GabrielFraga <gabrielbff2006@gmail.com>
2024-12-02 22:11:01 -0300

    q7

--- q7/app.js
+++ q7/app.js
@@ -7,7 +7,27 @@ const port = 8081
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+app.post('/calendario', (req,res) => {
+var {date} = req.body
 
+
+daty = new Date(date)
+
+var nextYear = new Date()
+
+daty.setDate(daty.getDate() + 1)
+console.log(daty.toString())
+
+nextYear.setFullYear(daty.getFullYear() + 1, 0, 1   )
+
+console.log(nextYear.toString())
+
+var diferencaMili = Math.abs(nextYear - daty)  
+var incomingDays = Math.floor((diferencaMili)/(1000 * 60 * 60 * 24))  
+
+res.json({message: `Faltam ${incomingDays} dias para o ano seguinte à data inserida!`})
+
+})
 
 
 app.get('/', (req,res) => {
